725b3d5934728b3b36a026ca6cec2d44038d5f1b
Zixun55 <b0979433390@gmail.com>
2026-07-09 11:17:44 +0800

    clip consistency更改閥值

--- consistency/clip_consistency.py
+++ consistency/clip_consistency.py
@@ -5,6 +5,6 @@ checker = CLIPConsistency()
 def get_response(image_path, caption, client=None):
     score = checker.get_similarity(image_path, caption)
     
-    verdict = "TRUE" if score >= 0.28 else "FALSE"
+    verdict = "TRUE" if score >= 0.285 else "FALSE"
     
     return f"<verdict>{verdict}</verdict> <score>{score}</score>"
